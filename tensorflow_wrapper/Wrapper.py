476b405c589687d029654b1e17b8307e2cd1d6ac
Andrea Schioppa <andrea.schioppa@booking.com>
2018-03-06 17:07:40 +0100

    wrapper v1

--- tensorflow_wrapper/Wrapper.py
+++ tensorflow_wrapper/Wrapper.py
@@ -54,12 +54,18 @@ class VowpalWabbitWrapper:
                                                             key_dtype=tf.string,
                                                         value_dtype=tf.float64),
                                                            default_value = 0.0)
-
+        
 
     def wrapModel(self):
         # dict of all tensors to be returned
         tensorDict = {}
-
+        # instantiate the most important tensor: the response
+        tensorDict["response"] = tf.get_variable("WrappedResponse",
+                                                 initializer = tf.constant(0.0,
+                                                                           shape=[1],dtype=tf.float64),dtype=tf.float64)
+        # add the Constant from Vowpal Wabbit
+        tensorDict["response"] += self.weightsTensor.lookup(tf.constant('Constant',tf.string))
+        
         tensorDict["Input"] = {}
         # create input tensors
         for namespace, features in self.feaDict.iteritems():
@@ -74,5 +80,81 @@ class VowpalWabbitWrapper:
                     tensorDict["Input"].update({feaname :
                                             tf.placeholder(tf.float64, [], name="Input_"+
                                                            re.sub("\W","_void_",feaname))})
+                    
+        # create parsed input: i.e. to lookup the value in the weights table
+        tensorDict["ParsedInput"] = {}
+        for namespace, features in self.feaDict.iteritems():
+            for feaname, featype in features.iteritems():
+                if featype is FeaType.categorical:
+                    tensorDict["ParsedInput"].update({feaname :
+                                              tf.string_join([tf.constant(namespace+'^'+feaname+'=',dtype=tf.string),tensorDict["Input"][feaname]],
+                                                             name="ParsedInput_"+re.sub("\W","_void_",feaname))})
+                elif featype is FeaType.numerical:
+                    tensorDict["ParsedInput"].update({feaname :
+                                                      tf.constant(namespace+'^'+feaname,dtype=tf.string)})
+        # extract weight(s) values (multiplied by feature value for numerical features)
+        tensorDict["Values"] = {}
+        for namespace, features in self.feaDict.iteritems():
+            for feaname, featype in features.iteritems():
+                if featype is FeaType.categorical:
+                    tensorDict["Values"].update({feaname : self.weightsTensor.lookup(tensorDict["ParsedInput"][feaname])})
+                elif featype is FeaType.numerical:
+                    tensorDict["Values"].update({feaname : tf.multiply(tensorDict["Input"][feaname],self.weightsTensor.lookup(
+                        tensorDict["ParsedInput"][feaname]))})
+                # add the value to the response
+                tensorDict["response"] += tensorDict["Values"][feaname]
+        
+        # break the interactionsString
+        interactionsDict = {}
+        if self.interactionsString is not None:
+            interactions = self.interactionsString.strip().split(',')
+            for interaction in interactions:
+                interactionsDict.update({interaction : interaction.split('*')})
 
+        # create tensors representing interactions
+        tensorDict["ParsedInteractions"] = {}
+        tensorDict["InteractionValues"] = {}
+        for interaction, namespaces in interactionsDict.iteritems():
+            tensorDict["ParsedInteractions"][interaction] = {}
+            tensorDict["InteractionValues"][interaction] = {}
+            # create programmatically lists of features to interact
+            feasList = []
+            for namespace in namespaces:
+                feasList.append([[{"namespace" : namespace,
+                                   "feaname" : k,
+                                   "featype": t}] for k,t in self.feaDict[namespace].iteritems()])
+            feasToInteract = reduce(VowpalWabbitWrapper.crossProductLists,feasList)
+            # for each interaction create tag & name_value
+            for fealist in feasToInteract:
+                tag = reduce(lambda y,z : y+'*'+z,map(lambda x: x["feaname"], fealist))
+                tensorDict["ParsedInteractions"][interaction][tag] = tf.string_join(
+                    map(lambda x: tensorDict["ParsedInput"][x["feaname"]],fealist),'*')
+                # first map categoricals to 1 & others to their value
+                tensorDict["InteractionValues"][interaction][tag] = reduce(lambda x,y : tf.multiply(x,y),
+                                                                           map(
+                                        lambda z: tensorDict["Input"][z["feaname"]]
+                                                if z["featype"] is FeaType.numerical
+                                                                               else tf.constant(1.0,dtype=tf.float64),
+                                                                               fealist))
+                # now take the product with the coefficients
+                tensorDict["InteractionValues"][interaction][tag] = tf.multiply(
+                    tensorDict["InteractionValues"][interaction][tag],self.weightsTensor.lookup(
+                                                                                tensorDict["ParsedInteractions"][interaction][tag]))
+                # add to the response
+                tensorDict["response"] += tensorDict["InteractionValues"][interaction][tag]
+        
+                                                                                
+                                                                               
         return tensorDict
+
+    """ Helper Method to cross product two lists 
+        Individual elements of the list are assumed to be lists
+    """
+    @staticmethod
+    def crossProductLists(l1, l2):
+        outList = []
+        for _l1 in l1:
+            for _l2 in l2:
+                outList.append(_l1 + _l2)
+        return outList
+            
